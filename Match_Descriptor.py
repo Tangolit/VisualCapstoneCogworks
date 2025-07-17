f244f6fa181f08a6f7e0c6f31722df0c197cd833
bella-qu <140536325+bella-qu@users.noreply.github.com>
2025-07-17 17:40:55 -0400

    Add files via upload

---
+++ Match_Descriptor.py
@@ -0,0 +1,14 @@
+#Functionality to see if a new descriptor has a match in your database, given the aforementioned cutoff threshold.
+#Siri
+
+threshold = "tbd" #find an appropriate threshold later
+
+def get_cosine_distance():
+    return #idk once this formula is made
+
+
+def is_a_match(descriptor):
+    if (get_cosine_distance(descriptor, ))
+
+
+
